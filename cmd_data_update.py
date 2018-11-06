436318923dfb03074f92c53e22b000e9d1c98ce4
Conrad <cgoffinet12@gmail.com>
2018-11-06 00:21:42 -0500

    Bug Fixed cmd_data_update
    Played two more games of pong

--- cmd_data_update.py
+++ cmd_data_update.py
@@ -7,7 +7,7 @@ data = pickle.load(open('data.p','rb'))
 
 while user_input != 'exit':
     user_input = input('Type a command: ')
-    input_list = user_input.lower().split()
+    input_list = user_input.split()
     
     if input_list[0] == 'name': # if user wishes to add a name
         if input_list[1] == 'add':
@@ -15,19 +15,20 @@ while user_input != 'exit':
             pickle.dump(data, open( "data.p", "wb" ))
         if input_list[1] == 'rm':
             data.remove_player(input_list[2])
-
+            pickle.dump(data, open( "data.p", "wb" ))
+            
     if input_list[0] == 'game':
         if input_list[1] == 'add':
             names = data.create_player_name_list()
             print('Here\'s a list of players', names)
             P1 = input('Winning Player 1 Name: ')
-            P1_score = input('Player 1 score: ')
+            P1_score = int(input('Player 1 score: '))
             P2 = input('Winning Player 2 Name: ')
-            P2_score = input('Player 2 score: ')
+            P2_score = int(input('Player 2 score: '))
             P3 = input('Losing Player 3 Name: ')
-            P3_score = input('Player 3 score: ')
+            P3_score = int(input('Player 3 score: '))
             P4 = input('Losing Player 4 Name: ')
-            P4_score = input('Player 4 score: ')
+            P4_score = int(input('Player 4 score: '))
             
             while P1 not in names:
                 P1 = input('Oops! Player 1 is not a valid  name. Try again: ') 
@@ -46,8 +47,8 @@ while user_input != 'exit':
                 P1_score = input('Oops! The scores of player 3 and 4 don\'t add up! Player 3 score: ')
                 P2_score = input('Player 4 score: ') 
 
-            confirmation = input('Winners: '+P1+':'+P1_score+', '+P2+':'+P2_score+',\n' +
-                                 'Losers: '+P3+':'+P3_score+', ' +P4+':'+P4_score+'\n'
+            confirmation = input('Winners: '+P1+':'+str(P1_score)+', '+P2+':'+str(P2_score)+',\n' +
+                                 'Losers: '+P3+':'+str(P3_score)+', ' +P4+':'+str(P4_score)+'\n'
                                  'Is this correct? (y/n) ')
             if confirmation.lower() == 'y':
                 team1 = ((data.fetch_player(P1), P1_score),(data.fetch_player(P2), P2_score))
